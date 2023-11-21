4230490f4b4f3eb669087c69bb27cabe102dd128
eneging <140635337+eneging@users.noreply.github.com>
2023-11-21 13:47:11 -0500

    navegacion

--- frontend/src/components/pages/studends.jsx
+++ frontend/src/components/pages/studends.jsx
@@ -14,7 +14,7 @@ const Students = () => {
     try {
 
       const response = await axios.get(`${API_URL}/docentes`);
-      console.log(students);
+      
 
       setStudents(response.data);
     } catch (error) {
@@ -34,7 +34,9 @@ const Students = () => {
 
   return (
     <div>
-      <h1>List of Students</h1>
+  <div className='flex justify-between p-[1rem]'>
+        <h1 className='text-center text-xl  p-[0.5rem] '>Lista de estudiantes</h1><button className="bg-blue-500 text-white rounded-[5px] p-[0.5rem]">Crear Estudiante</button>
+      </div>
       {/* <ul>
         {students.map((student) => (
           <li key={student.docente_id}>
@@ -83,183 +85,7 @@ const Students = () => {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-10 w-10">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://i.pravatar.cc/150?img=1"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    Jane Cooper
-                  </div>
-                  <div className="text-sm text-gray-500">
-                    jane.cooper@example.com
-                  </div>
-                </div>
-              </div>
-            </td>
-
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Estudiante
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              jane.cooper@example.com
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-
-              <a href="/editar" className="text-indigo-600 hover:text-indigo-900">
-
-                Editar
-              </a>
-              <a href="#" className="ml-2 text-red-600 hover:text-red-900">
-                Eliminar
-              </a>
-            </td>
-          </tr>
-
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-10 w-10">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    Carlos Garcia
-                  </div>
-                  <div className="text-sm text-gray-500">carlos@mail.com</div>
-                </div>
-              </div>
-            </td>
-
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Estudiante
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              carlos@mail.com
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-
-              <a href="/editar" className="text-indigo-600 hover:text-indigo-900">
-
-
-                Editar
-              </a>
-              <a href="#" className="ml-2 text-red-600 hover:text-red-900">
-                Eliminar
-              </a>
-            </td>
-          </tr>
-
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-10 w-10">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    Ivan Morris
-                  </div>
-                  <div className="text-sm text-gray-500">ivan@mail.com</div>
-                </div>
-              </div>
-            </td>
-
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Estudiante
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              ivan@mail.com
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-
-              <a
-                href="/editar"
-                className="text-indigo-600 hover:text-indigo-900"
-              >
-
-                Editar
-              </a>
-              <a href="#" className="ml-2 text-red-600 hover:text-red-900">
-                Eliminar
-              </a>
-            </td>
-          </tr>
-
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-10 w-10">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    Fernando Morales
-                  </div>
-                  <div className="text-sm text-gray-500">fernan@mail.com</div>
-                </div>
-              </div>
-            </td>
-
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Estudiante
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              fernan@mail.com
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-
-              <a
-                href="/editar"
-                className="text-indigo-600 hover:text-indigo-900"
-              >
-
-                Editar
-              </a>
-              <a href="#" className="ml-2 text-red-600 hover:text-red-900">
-                Eliminar
-              </a>
-            </td>
-          </tr>
-
+          
           {/* PRUEBA */}
 
           {students.map((student) => (
--- frontend/src/components/routes/navigation.jsx
+++ frontend/src/components/routes/navigation.jsx
@@ -2,32 +2,33 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav className="bg-gray-800  text-white flex justify-around h-[10vh] w-screen items-center ">
-      <div>#mi app matriculas</div>
+    <nav className="bg-gray-800  text-white flex justify-around h-[12vh] w-screen items-center ">
+      <div className="flex gap-4 items-center bg-orange-500 p-1 "><img  className="w-[3vw]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6G3iW4Ur14DPKXx427YVLoMAv83QjhgOIgZdzNO7Tm5M6meiVyRg3sMQlsHT35rptSIE&usqp=CAU"></img>  
+      <h1 className="flex justify-center items-center font-serif">Universidad <br></br>del  Atlantico</h1></div>
 
       
       <ul className='flex justify-around w-[60%]'>
 
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/"><h1 className="hover:text-orange-500">Home</h1></Link>
         </li>
         <li>
 
   
 
-          <Link to="/students">Registro Estudiantes</Link>
+          <Link to="/students"><h1 className="hover:text-orange-500">Registro Estudiantes</h1></Link>
         </li>
         <li>
-          <Link to="/teachers">Registro Docentes</Link>
+          <Link to="/teachers"><h1 className="hover:text-orange-500">Registro Docentes</h1></Link>
 
         </li>
 
         <li>
-          <Link to="/carreras">Registro Carreras</Link>
+          <Link to="/carreras"><h1 className="hover:text-orange-500">Registro Carreras</h1></Link>
         </li>
 
         <li>
-          <Link to="/matriculas">Registro matriculas</Link>
+          <Link to="/matriculas"><h1 className="hover:text-orange-500">Registro matriculas</h1></Link>
         </li>
       </ul>
     </nav>
