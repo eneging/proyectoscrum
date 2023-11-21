445e4d383e182267bb467b0c43ecad3a4578ec01
freirediego <diegofg23@outlook.es>
2023-11-21 10:42:11 -0500

    diego-front

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -1,11 +1,5 @@
-
-
 function Home() {
-  return (
-    <main className="bg-red-200">
-ff
-    </main>
-  )
+  return <main className="bg-red-200">fff</main>;
 }
 
-export default Home
+export default Home;
