6a9aeafa2d58f51de1cd85f97a3f120640da6b42
freirediego <diegofg23@outlook.es>
2023-11-21 01:15:44 -0500

    Vistas alumnos, maestros, y editar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,31 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/home";
 
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/home';
+import Teachers from "./components/pages/teachers";
+import Navigation from "./components/routes/navigation";
 
-import Teachers from './components/pages/teachers';
-import Navigation from './components/routes/navigation';
+import Students from "./components/pages/studends";
+import Carreras from "./components/pages/carreras";
 
-import Students from './components/pages/studends';
-import Carreras from './components/pages/carreras';
+import Editar from "./components/pages/editar";
 
 function App() {
-
-
   return (
     <>
-    
-    <Router>
-      <Navigation></Navigation>
-    
-    <Routes>
-   <Route path="/" element={<Home />} />
-      <Route path="/students" element={<Students />} />
-      <Route path="/teachers" element={<Teachers />} />
-      <Route path="/carreras" element={<Carreras />} />
-    </Routes>
-  </Router>
+      <Router>
+        <Navigation></Navigation>
 
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/teachers" element={<Teachers />} />
+          <Route path="/carreras" element={<Carreras />} />
+          <Route path="/editar" element={<Editar />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/routes/navigation.jsx
+++ frontend/src/components/routes/navigation.jsx
@@ -1,37 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-<nav className='bg-gray-800  text-white flex justify-around h-[10vh] w-screen items-center ' >
+    <nav className="bg-gray-800  text-white flex justify-around h-[10vh] w-screen items-center ">
       <div>#mi app matriculas</div>
-      
-      <ul className='flex justify-around w-[40%]'>
+
+      <ul className="flex justify-around w-[40%]">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/teachers">studiantes</Link>
+          <Link to="/students">studiantes</Link>
         </li>
         <li>
-          <Link to="/students">docentes</Link>
+          <Link to="/teachers">docentes</Link>
         </li>
 
         <li>
           <Link to="/carreras">carreras</Link>
         </li>
       </ul>
-
-
-
-
-      
     </nav>
-
-    
-
-
   );
-  
 }
 
 export default Navigation;
