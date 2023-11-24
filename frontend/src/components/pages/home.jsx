94f10b3fba15d46bd1be3f7b8d3f3576daf94a30
freirediego <diegofg23@outlook.es>
2023-11-24 15:37:30 -0500

    test

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -1,37 +1,24 @@
 function Home() {
-
   return (
-<>  
-<div  className="bg-gray-800  text-white flex justify-around h-[12vh] w-screen items-center "></div>
-  <main className=" h-screen bg-[url('https://www.unica.edu.pe/estilosweb/images/bg/bg2nuevo.jpg')]">
+    <>
+      <div className="bg-gray-800  text-white flex justify-around h-[12vh] w-screen items-center "></div>
+      <main className=" h-screen bg-[url('https://www.unica.edu.pe/estilosweb/images/bg/bg2nuevo.jpg')]"></main>
+      <div className="bg-white h-screen"></div>
+      <footer className="h-[10vh] bg-orange-500  ">
+        <div className="flex justify-around text-white ">
+          <div className="flex gap-2">
+            <div>(056) 284399</div>
+            <div> LUN-VIE 8:00 a 15:30</div>
+            <div> mesadepartes@unica.edu.pe</div>
+          </div>
 
-    
-
-
-</main>
-<div className="bg-white h-screen"></div>
-<footer className="h-[20vh] bg-orange-500  ">
-
-<div className="flex justify-around text-white ">
-<div className="flex gap-2">
-<div>(056) 284399</div>
-<div> LUN-VIE 8:00 a 15:30</div>
-<div> mesadepartes@unica.edu.pe</div>
-</div>
-
-
-<div className="flex gap-2">
-<div>Bolsa de Trabajo</div>|
-<div>Correos</div>|
-<div>Soporte</div>
-</div>
-</div>
-
-
-</footer>
-</>
-
-);}
-
+          <div className="flex gap-2">
+            <div>Bolsa de Trabajo</div>|<div>Correos</div>|<div>Soporte</div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
 
 export default Home;
